6b0f1b677e1aa22e0f3e6377a2e733177d657f47
elif sahin <sahinelif.mail@gmail.com>
2017-07-25 17:16:10 +0300

    modeller oluşturuldu

--- oneri/urun/models.py
+++ oneri/urun/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class Product(models.Model):
+    customer = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    vendor = models.CharField(max_length=200)
+    comment = models.TextField()
+    image = models.ImageField()
+    score = models.SmallIntegerField()
+    price = models.SmallIntegerField
+    performance = models.SmallIntegerField()
+    design = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
 # Create your models here.
