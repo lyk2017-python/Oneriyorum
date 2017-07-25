0e68e4287a81eddfec32c877d9da34ab6111da38
elif sahin <sahinelif.mail@gmail.com>
2017-07-25 21:16:50 +0300

    Modeller duzenlendi

--- oneri/urun/models.py
+++ oneri/urun/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Vendor(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "#{id} {name}".format(id=self.id, name=self.name)
+
 class Product(models.Model):
     vendor = models.ManyToManyField("Vendor")
     name = models.CharField(max_length=200)
@@ -12,8 +15,8 @@ class Comment(models.Model):
 
     vendor = models.ForeignKey('Product')
     comment = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(blank=True, null=True)
     price = models.SmallIntegerField()
     performance = models.SmallIntegerField()
-    design = models.SmallIntegerField()
+    design = models.SmallIntegerField(default=0)
 
