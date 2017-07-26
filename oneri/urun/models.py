a5d8b9705e3d542d43518bc99933c16bdd845405
elif sahin <sahinelif.mail@gmail.com>
2017-07-26 17:15:15 +0300

    Modeller yenilendi

--- oneri/urun/models.py
+++ oneri/urun/models.py
@@ -8,16 +8,18 @@ class Vendor(models.Model):
         return "{name}".format(name=self.name)
 
 class Product(models.Model):
+    """This class is written for user's comments. The comments have to include an image"""
     vendor = models.ManyToManyField("Vendor")
     name = models.CharField(max_length=200)
-    image = models.ImageField(default=None, null=True)
-
-class Comment(models.Model):
-    """This class is written for user's comments. The comments have to include an image"""
-    vendor = models.ForeignKey('Product')
     comment = models.TextField()
     image = models.ImageField(blank=True, null=True)
     price = models.SmallIntegerField()
     performance = models.SmallIntegerField()
     design = models.SmallIntegerField(default=0)
 
+class Comment(models.Model):
+
+    vendor = models.ForeignKey('Product')
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+    other_comment = models.TextField()
