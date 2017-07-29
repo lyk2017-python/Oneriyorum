c8f09ef6d0b5409a3faae7477ce5c984accdab9c
Burcu Altınok <burcualtnok@gmail.com>
2017-07-29 08:26:46 +0000

    commentCreate eklendi

--- oneri/urun/urls.py
+++ oneri/urun/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from oneri.views import ProductDetailView, AnasayfaView, SssView, ProductCreate
+from oneri.views import ProductDetailView, AnasayfaView, SssView, ProductCreate, CommentCreate
 
 urlpatterns=[
     url(r"^$", AnasayfaView.as_view(), name="Anasayfa"),
     url(r"^product/(?P<pk>\d+)$", ProductDetailView.as_view(), name="product"),
     url(r"sss$", SssView.as_view()),
     url(r'product/add/$', ProductCreate.as_view(), name='product-add'),
+    url(r'comment/add/$', CommentCreate.as_view(), name='comment-add'),
 ]
